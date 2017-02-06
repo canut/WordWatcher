65f332d89e25187ce610cf0885b2aa521797f694
canut <michael@maudits.com>
2017-02-06 23:37:32 +0100

    Implement a "Console Watcher" that will watch the Console for URL to download

--- WordWatcher/Program.cs
+++ WordWatcher/Program.cs
@@ -37,7 +37,7 @@ namespace WordWatcher
             try
             {
                 Task<WebResponse> T_response = request.GetResponseAsync();
-                Console.WriteLine("Downloading while in method...");    // this doesn't wait for the result of GetResponseAsync()
+                Console.WriteLine("Downloading {0}...", Url);    // this doesn't wait for the result of GetResponseAsync()
                 WebResponse response = await T_response;
 
                 // Read response
@@ -76,22 +76,40 @@ namespace WordWatcher
             CountWord();
             Console.WriteLine("There is {0} words in my page {1}", WordCounter, Url);
         }
+
+        public void ConsoleWatcher()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+                Url = input;
+            } while (Url != "stop");
+        }
+
+        public async Task ConsoleWatcherAsync()
+        {
+            Console.WriteLine("ConsoleWatcher Thread started");
+            await Task.Run(new Action(ConsoleWatcher));
+            Console.WriteLine("ConsoleWatcher Thread stopped");
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            List<Page> UrlList = new List<Page> {
-                                    new Page("http://www.google.fr"),
-                                    new Page("http://www.facebook.fr"),
-                                    new Page("http://www.lemonde.fr"),
-                                    new Page("http://www.meteofrance.fr")
-                                    };
+            Page p = new Page("http://www.google.fr");
+            string last_url = "";
 
-            IEnumerable<Task> downloadTasksQuery = from page in UrlList select page.ProcessURLAsync();
-            Task[] downloadTasks = downloadTasksQuery.ToArray();
-            Task.WaitAll(downloadTasks);        // OK for void returning Task but how to do it it Task<int> is returned
+            p.ConsoleWatcherAsync();
+            
+            while (p.Url != "stop") {
+                if (p.Url != last_url) {
+                    Task.WaitAll(p.ProcessURLAsync());
+                    last_url = p.Url;
+                }
+            }
+            
             Console.WriteLine("Press enter to exit...");
             Console.ReadKey();
         }
